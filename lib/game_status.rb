316af2eeb6b721a1dd05ca2dcbd2bb4ed832f648
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-09 20:08:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -54,12 +54,9 @@ def over?(board)
   end
 end
 
-def winner (board)
+def winner(board)
   index = []
   index = won?(board)
-  if index == false
-    return nil
-  else
     if board[index[0]] == "X"
       return "X"
     else
