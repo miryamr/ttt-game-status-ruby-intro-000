b1a5b58751674abd28cff2db67db798405a311f6
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-09 21:17:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,18 +21,24 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.detect do |winning_array|
-  all_X = winning_array.all? do |single_index| #we've created all_X variable because .detect will only work with the last condition that was entered which means it would only iterate over "O" - therefore by creating the variables all_X and all_O I'm making the conditions one unit under which i call .detect on line 30
-    board[single_index] == "X" #board[single_index] is referring to each index on the board only considering the winning_array/WIN_COMBINATIONS
+    board[winning_array[0]] == board[winning_array[1]] && board[winning_array[1]] == board[winning_array[2]] && position_taken?(board, winning_array[0])
   end
-  all_O = winning_array.all? do |single_index|
-    board[single_index] == "O"
-  end
-all_X == true || all_O == true #this means if any of the arrays matches the conditions
-
 end
+
+
+
+#all_X = winning_array.all? do |single_index| #we've created all_X variable because .detect will only work with the last condition that was entered which means it would only iterate over "O" - therefore by creating the variables all_X and all_O I'm making the conditions one unit under which i call .detect on line 30
+  #  board[single_index] == "X" #board[single_index] is referring to each index on the board only considering the winning_array/WIN_COMBINATIONS
+#  end
+#  all_O = winning_array.all? do |single_index|
+#    board[single_index] == "O"
+#  end
+#all_X == true || all_O == true #this means if any of the arrays matches the conditions
+
+#end
 #this is, within the winning combinations, does every index on the board match with either X or O?
   #  return false
-  end
+#  end
 
 def full?(board)
   board.all? do |each_index|
